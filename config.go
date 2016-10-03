2e592dd5969e38deb78390016d56bd20432be8fc
zheng-ji <407562752@qq.com>
2016-10-03 21:47:28 +0800

    add proxyconfig available

--- config.go
+++ config.go
@@ -3,10 +3,10 @@
 package main
 
 import (
-	//"errors"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"launchpad.net/goyaml"
+	"os"
 )
 
 type ProxyConfig struct {
@@ -24,14 +24,35 @@ type LogConfig struct {
 	Path  string `yaml:"path"`
 }
 
+func (lc *LogConfig) isValid() bool {
+	_, err := os.Stat(lc.Path)
+	return err == nil || os.IsExist(err)
+}
+
 func parseConfigFile(filepath string) error {
 	if config, err := ioutil.ReadFile(filepath); err == nil {
 		if err = goyaml.Unmarshal(config, &pConfig); err != nil {
 			return err
 		}
-		fmt.Println(pConfig)
+		if pConfig.isValid() == false {
+			err := errors.New("wrong config")
+			return err
+		}
+		if pConfig.FailOver <= 0 {
+			pConfig.FailOver = 5
+		}
+		if pConfig.MaxConn <= 0 {
+			pConfig.MaxConn = 5
+		}
+		if pConfig.Timeout <= 0 {
+			pConfig.Timeout = 5
+		}
 	} else {
 		return err
 	}
 	return nil
+}
+
+func (pc *ProxyConfig) isValid() bool {
+	return len(pc.Backend) > 0 && pc.Log.isValid()
 }
