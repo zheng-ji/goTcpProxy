0963dc1e844face0dc28adc60df1a3f59a14cca9
zj <zj@zhengji>
2016-10-24 09:00:27 +0800

    mksure log dir exist

--- log.go
+++ log.go
@@ -5,9 +5,15 @@ package main
 import (
 	"github.com/Sirupsen/logrus"
 	"os"
+	"path/filepath"
 )
 
 func initLogger() error {
+	dirPath, _ := filepath.Abs(filepath.Dir(pConfig.Log.Path))
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		os.Mkdir(dirPath, 0775)
+	}
+
 	file, err := os.OpenFile(pConfig.Log.Path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 
 	if err != nil {
